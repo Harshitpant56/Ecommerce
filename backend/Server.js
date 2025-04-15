f1f3d52932a1c55cd1d506012d6d44692e9d19d7
Cobrakainosi <apple@Nilesh-Personal.local>
2025-04-15 12:40:33 +0530

    Initial commit

--- backend/Server.js
+++ backend/Server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import path from 'path';
 import paymentRoutes from "./routes/payment.route.js"
 import analyticsRoute from "./routes/analytics.route.js"
 import { connectDB } from "./lib/db.js";
@@ -14,6 +15,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
 
@@ -23,6 +26,15 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments",paymentRoutes);
 app.use("/api/analytics",analyticsRoute)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
